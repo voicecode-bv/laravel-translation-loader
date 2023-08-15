d4bf52d636060cdea19735c492eac54cfc27bfe8
jordyvanderhaegen <jordy@esign.eu>
2023-08-15 17:29:48 +0200

    Add: translator facade

---
+++ src/Facades/Translator.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Esign\TranslationLoader\Facades;
+
+use Esign\TranslationLoader\Translator as TranslationLoaderTranslator;
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @method static void setMissingKeyCallback(Closure $callback)
+ * @method static void createMissingTranslations()
+ */
+class Translator extends Facade
+{
+    protected static function getFacadeAccessor(): string
+    {
+        return TranslationLoaderTranslator::class;
+    }
+}
